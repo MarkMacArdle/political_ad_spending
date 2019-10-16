6a7a0844bede815de3d22fb41dbdad74089d3ad7
MarkMacArdle <magardle@yahoo.co.uk>
2019-10-16 18:26:31 +0100

    Add config file

---
+++ config.py
@@ -0,0 +1,26 @@
+search_fields = [
+    'ad_creation_time',
+    'ad_delivery_start_time',
+    'ad_delivery_stop_time',
+    'ad_creative_body',
+    'ad_creative_link_title',
+    'ad_creative_link_caption',
+    'ad_creative_link_description',
+    'page_id',
+    'page_name',
+    'currency',
+    'spend',  # returns range in dict
+    'demographic_distribution',  # returns list of dicts
+    'funding_entity',
+    'impressions',  # returns range in dict
+    'region_distribution',  # returns list of dicts
+]
+
+# Use the graph api to get a new token
+#       https://developers.facebook.com/tools/explorer
+# That token will expire in about an hour so use the debugger to extend it and
+# up to two months.
+#       https://developers.facebook.com/tools/debug/accesstoken
+# helpful post on getting access tokens:
+#       https://medium.com/@DrGabrielA81/python-how-making-facebook-api-calls-using-facebook-sdk-ea18bec973c8#6fdb
+user_access_token_extended_expiry = 'your token here'
